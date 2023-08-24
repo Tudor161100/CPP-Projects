214e8c90d6313cdc448673df8c59bb46ad2a701a
Tudor161100 <tudormarmureanunicolae@gmail.com>
2023-08-24 18:25:51 +0300

    Update 250.cpp

--- pbinfo/250.cpp
+++ pbinfo/250.cpp
@@ -1,48 +1,47 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int n, m, k=0, a[100001], b[100001], c[100001], i, j;
-
 int main()
 {
-    cin>>n;
-    for(i=1;i<=n;i++) cin>>a[i];
-    cin>>m;
-    for(j=1;j<=m;j++) cin>>b[j];
+    int n, m, k = 0, i. j, a[100001], b[100001], c[200001];
+    cin >> n;
+    for(i=1;i<=n;i++)
+        cin >> a[i];
+    cin >> m;
+    for(j=1;j<=n;j++)
+        cin >> b[j];
 
-    while(i<=n && j<=m){
-        if(a[i]<b[j])
-            {
-                c[++k]=a[i];
+    while(i <= n && j <= m)
+        if(a[i] < b[j]){
+            c[++k] = a[i];
+			i++;
+		}
+        else
+            if(a[i] > b[j])
+			{
+				c[++k] = b[j];
+				j++;
+			}
+            else
                 i++;
-            }
-        else 
-        {
-            if(a[i]>b[j])
-            {
-                c[++k]=b[j];
-                j++;
-            }
-            else i++;
-        }
-    }
 
-    while(i<=n){
-        c[++k]=a[i];
-        i++;
-    }
+    while(i <= n)
+	{
+		c[++k] = a[i];
+		i++;
+	}
 
-    while(j<=m){
-        c[++k]=b[j];
-        j++;
-    }
+    while(j <= m)
+	{
+		c[++k] = b[j];
+		j++;
+	}
 
-    for(i=1;i<=k;i++)
-    {
-        cout<<c[i]<<' ';
-        if(i%10==0)
-            cout<<endl;
+    for(i=1;i<=n;i++){
+        cout << c[i] << " ";
+        if(i % 10 == 0)
+            cout << endl;
     }
 
     return 0;
